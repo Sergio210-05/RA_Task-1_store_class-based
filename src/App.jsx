75ae7985801bef6844c768501f8d35cb11b390db
Sergei Snitko <sergeisnit@yandex.ru>
2024-04-16 00:44:30 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styles from './App.module.css'
+import { ShopItemClass } from './components/shop/ShopItem'
+
+function App() {
+
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£',
+  }
+
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["background-element"]}>
+      </div>
+      <div className={styles["highlight-window"]}>
+        <div className={styles['highlight-overlay']}></div>
+      </div>
+      <div className={styles["window"]}>
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+  )
+}
+
+export default App
